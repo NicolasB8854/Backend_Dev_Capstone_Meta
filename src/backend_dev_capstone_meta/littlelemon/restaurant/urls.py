2392ac4b4b7315ffaf277bc1cddff4c61e8b60f5
NicolasB8854 <nicolasbogun@gmail.com>
2024-08-07 22:01:00 +0200

    feat: Implement routing for APIs

--- src/backend_dev_capstone_meta/littlelemon/restaurant/urls.py
+++ src/backend_dev_capstone_meta/littlelemon/restaurant/urls.py
@@ -1,11 +1,15 @@
-from django.urls import path
+from django.urls import path, include
 from .views import MenuItemsView, SingleMenuItemView
 from . import views
 from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework.routers import DefaultRouter
 
+router = DefaultRouter()
+router.register(r'tables', views.BookingViewSet, basename='booking')
 
 urlpatterns = [
-    path('menu-items/', views.MenuItemsView.as_view()),
+    path('menu-items/', views.MenuItemsView.as_view(), name='menu'),
     path('menu-items/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('', include(router.urls)),
     path('api-token-auth/', obtain_auth_token),
 ]
